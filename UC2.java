d60636fd7361b99fbf0d97c7ca20251f62589204
technology-97 <rushikeshbhandare789@gmail.com>
2021-07-31 13:22:40 +0600

    Added:UC2_AddingNodes program is Added

---
+++ UC2.java
@@ -0,0 +1,31 @@
+public class UC2 {
+
+            public static <K, myThirdNode> void main(String[] args) {
+                INode myFirstNode = new  INode(70);
+                INode mySecondNode =new  INode(30);
+                INode myThirdNode = new  INode(50);
+
+                MyLinkedList myLinkedList = new MyLinkedList();
+                myLinkedList.add(myFirstNode);
+                myLinkedList.add(mySecondNode);
+                myLinkedList.add ((myThirdNode);
+
+                myLinkedList.printNodes();
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
